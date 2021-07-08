695608dee4f3a6407a68fa1cfb70f9115c9a49e5
swg <suweigao1@gmail.com>
2021-07-08 20:51:22 +0800

    uploaded BufferTimeRule.js. TODO: event listener + BufferLevel adjustment
    Signed-off-by: swg <suweigao1@gmail.com>

--- web/js_files/BufferTimeRule.js
+++ web/js_files/BufferTimeRule.js
@@ -29,58 +29,54 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 
-var LowestBitrateRule;
+var BufferTimeRule;
 
 // Rule that selects the lowest possible bitrate
-function LowestBitrateRuleClass() {
+function BufferTimeRuleClass() {
 
     let factory = dashjs.FactoryMaker;
     let SwitchRequest = factory.getClassFactoryByName('SwitchRequest');
     let MetricsModel = factory.getSingletonFactoryByName('MetricsModel');
     let StreamController = factory.getSingletonFactoryByName('StreamController');
+    let DashMetrics = factory.getSingletonFactoryByName('DashMetrics');
     let context = this.context;
+    let timeSpan = -1;
     let instance;
 
-    function setup() {
+    function setup(time) {
+        timeSpan = time;
     }
 
     // Always use lowest bitrate
     function getMaxIndex(rulesContext) {
-        // here you can get some informations about metrics for example, to implement the rule
+        // here you can get some informations aboit metrics for example, to implement the rule
         let metricsModel = MetricsModel(context).getInstance();
-        var mediaType = rulesContext.getMediaInfo().type;
-        var metrics = metricsModel.getMetricsFor(mediaType, true);
-
-
+        let dashMetrics = DashMetrics(context).getInstance();
+        let mediaType = rulesContext.getMediaInfo().type;
+        let metrics = metricsModel.getMetricsFor(mediaType, true);
+        let bufferLevel = dashMetrics.getCurrentBufferLevel(mediaType);
         // A smarter (real) rule could need analyze playback metrics to take
         // bitrate switching decision. Printing metrics here as a reference
-        // console.log(metrics);
+        console.log({"buflev": bufferLevel});
 
 
         // Get current bitrate
         let streamController = StreamController(context).getInstance();
         let abrController = rulesContext.getAbrController();
-        let current = abrController.getQualityFor(mediaType, streamController.getActiveStreamInfo());
+        let current = abrController.getQualityFor(mediaType, streamController.getActiveStreamInfo().id);
 
-        let mediaInfo = rulesContext.getMediaInfo();
-        let throughput = abrController.getThroughputHistory().getAverageThroughput(mediaType);
-        let timeStamp = 0;
-
-        console.log({"metrics": metrics, "throughput": throughput, "timestamp": timeStamp});
         // If already in lowest bitrate, don't do anything
-        if (current === 0) {
-            return SwitchRequest(context).create();
+        if (timeSpan > 0 || current === 0) {
+            let sr = SwitchRequest(context).create();
+            return sr;
+            // return SwitchRequest(context).create();
         }
 
-        // Ask to switch to the lowest bitrate
+        // Ask to fulfill the buffer
         let switchRequest = SwitchRequest(context).create();
-        switchRequest.quality = 0;
-        switchRequest.reason = 'Always switching to the lowest bitrate';
+        switchRequest.quality = 0;//-1 is no change
+        switchRequest.reason = 'bad window in coming';
         switchRequest.priority = SwitchRequest.PRIORITY.STRONG;
-
-        let bufferRequest = Buffer
-
-
         return switchRequest;
     }
 
@@ -93,6 +89,6 @@ function LowestBitrateRuleClass() {
     return instance;
 }
 
-LowestBitrateRuleClass.__dashjs_factory_name = 'LowestBitrateRule';
-LowestBitrateRule = dashjs.FactoryMaker.getClassFactory(LowestBitrateRuleClass);
+BufferTimeRuleClass.__dashjs_factory_name = 'BufferTimeRule';
+BufferTimeRule = dashjs.FactoryMaker.getClassFactory(BufferTimeRuleClass);
 
